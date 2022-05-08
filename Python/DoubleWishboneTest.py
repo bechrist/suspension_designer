625b57795d9dba36ad1b1a7759e8648810c7ee8a
bechrist <40005251+bechrist@users.noreply.github.com>
2022-05-08 13:46:30 -0500

    Rework Target Dict

--- Python/DoubleWishboneTest.py
+++ Python/DoubleWishboneTest.py
@@ -5,44 +5,54 @@
 import numpy as np
 
 # %% Targets
-Target         = dict()
-Target['CG']   = np.zeros(3)
-Target['Type'] = dict()
-
 ## Vehicle Targets
-Target['Wheelbase']    = 1525             # Nominal Wheelbase               [mm]
-Target['WeightDist']   = 0.5              # Front Weight Distribution         []
-Target['SprungMass']   = 225              # Sprung Mass                     [kg]
-Target['CG'][2]        = 8.50*(25.4)      # Nominal CG Height           [in->mm]
-Target['Ride']         = 2   *(25.4)      # Nominal Ride Height         [in->mm]
-Target['Rake']         = 0   *(np.pi/180) # Nominal Rake Angle        [deg->rad]
-Target['LoadedRadius'] = 7.85*(25.4)      # Nominal Loaded Radius       [in->mm]
+Vehicle = dict()
+Vehicle['CG'] = np.zeros(3)
 
-Target['CG'][0] = Target['Wheelbase']*(1-Target['WeightDist']) # CG to Axle [mm]
+Vehicle['Wheelbase']    = 1525             # Nominal Wheelbase              [mm]
+Vehicle['WeightDist']   = 0.5              # Front Weight Distribution        []
+Vehicle['SprungMass']   = 225              # Sprung Mass                    [kg]
+Vehicle['CG'][2]        = 8.50*(25.4)      # Nominal CG Height          [in->mm]
+Vehicle['Ride']         = 2   *(25.4)      # Nominal Ride Height        [in->mm]
+Vehicle['Rake']         = 0   *(np.pi/180) # Nominal Rake Angle       [deg->rad]
+Vehicle['LoadedRadius'] = 7.85*(25.4)      # Nominal Loaded Radius      [in->mm]
+
+## Suspension Type
+Type = dict()
+
+Type['Axle']    = "Front"           # Axle
+Type['Linkage'] = "Double Wishbone" # Linkage Type
+Type['Strut']   = "Lower Rod"       # Strut Type
 
 ## Double Wishbone Targets
+Linkage = dict()
 
-Target['Type']['Linkage'] = "Double Wishbone"    # Suspension Linkage Type
+Linkage['Track']       =  1220                    # Front Track Width       [mm]
 
-Target['Track']       =  1220                    # Front Track Width        [mm]
+Linkage['Toe']         =  0.50*(np.pi/180)        # Static Toe (+Out) [deg->rad]
 
-Target['Toe']         =  0.50*(np.pi/180)        # Static Toe (+Out)  [deg->rad]
+Linkage['PitchCenter'] =  10                      # Normalized FBPC Height   [%]
+Linkage['Caster']      =  3.00*(np.pi/180)        # Static Caster     [deg->rad]
+Linkage['CasterGain']  =  0.25*(np.pi/(180*25.4)) # Caster Gain [deg/in->rad/mm]
 
-Target['PitchCenter'] =  10                      # Normalized FBPC Height    [%]
-Target['Caster']      =  3.00*(np.pi/180)        # Static Caster      [deg->rad]
-Target['CasterGain']  =  0.25*(np.pi/(180*25.4)) # Caster Gain  [deg/in->rad/mm]
+Linkage['RollCenter']  =  15                      # Normalized FBRC Height   [%]
+Linkage['Camber']      = -1.60*(np.pi/180)        # Static Camber     [deg->rad]
+Linkage['CamberGain']  = -1.00*(np.pi/(180*25.4)) # Camber Gain [deg/in->rad/mm]
 
-Target['RollCenter']  =  15                      # Normalized FBRC Height    [%]
-Target['Camber']      = -1.60*(np.pi/180)        # Static Camber      [deg->rad]
-Target['CamberGain']  = -1.00*(np.pi/(180*25.4)) # Camber Gain  [deg/in->rad/mm]
+Linkage['Scrub']       =  0.50*(25.4)             # Maximum Scrub       [in->mm]
+Linkage['KPI']         =  3.00*(np.pi/180)        # Target KPI        [deg->rad]
 
-Target['Scrub']       =  0.50*(25.4)             # Maximum Scrub        [in->mm]
-Target['KPI']         =  3.00*(np.pi/180)        # Target KPI         [deg->rad]
+## Pull / Pushrod Targets
+Strut = dict()
 
-"""
-Target['RideRatio']   =  0.80                    # Ride Motion Ratio Target   []
-Target['ARBRatio']    =  0.80                    # ARB Motion Ratio Target    []
-"""
+Strut['RideRatio']     =  0.80                    # Ride Motion Ratio Target  []
+Strut['ARBRatio']      =  0.80                    # ARB Motion Ratio Target   []
+
+## Compile Target Dictionary
+Target = {"Vehicle": Vehicle, 
+          "Type"   : Type   , 
+          "Linkage": Linkage,
+          "Strut"  : Strut  }
 
 # %% Double Wishbone Design Space Bounds
 # Notes:
@@ -66,8 +76,8 @@ Bound['TA']  = np.array([[  2.00,  3.00],[  8.70,  8.70],[  2.50,  2.75]]) # (X)
 
 """
 Bound['RA'] = np.array([[- 3.00,  2.00],[  6.00,  9.50],[  8.00, 10.00]]) # (X)
-Bound['PA'] = [  NaN ,  NaN ;  2.00,  4.00;  NaN ,  NaN ]*(25.4) # (R)
-Bound['SA'] = [- 3.00,  0.00;  8.00, 12.00; 10.00, 18.00]*(25.4) # (X)
+Bound['PA'] = np.array([[np.nan,np.nan],[  2.00,  4.00],[np.nan,np.nan]]) # (R)
+Bound['SA'] = np.array([[- 3.00,  0.00],[  8.00, 12.00],[ 10.00, 18.00]]) # (X)
 """
 
 # Outboard Pickups:    | Longitudinal  |    Lateral    |    Vertical   |
@@ -76,6 +86,16 @@ Bound['UB'] = np.array([[  0.00,  0.00],[- 1.75,- 0.88],[  3.00,  3.50]]) # (W)
 Bound['TB'] = np.array([[  2.50,  2.85],[- 1.25,- 0.88],[- 1.50,  0.50]]) # (W)
 
 """
-Bound['PB'] = [  2.25,  3.75;- 3.00,- 2.00;  1.50,  2.50]*(25.4) # (A) 
-Bound['SB'] = [  NaN ,  NaN ;  2.25,  3.75;  1.50,  2.50]*(25.4) # (R)
+Bound['PB'] = np.array([[  2.25,  3.75],[- 3.00,- 2.00],[  1.50,  2.50]]) # (A) 
+Bound['SB'] = np.array([[np.nan,np.nan],[  2.25,  3.75],[  1.50,  2.50]]) # (R)
 """
+
+# %% Linkage Design
+## Initialize Linkage
+
+## Sample Design Space
+
+## Static Design Generation
+
+# %% Linkage Simulation
+## Jounce & Steer Sweep
